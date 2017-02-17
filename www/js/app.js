655552f713b93c1e58a814d25be5fc6edc3fc193
Mauricio Pacheco <mpacheco12@gmail.com>
2017-02-17 17:50:42 -0500

    android build remote url

--- www/js/app.js
+++ www/js/app.js
@@ -42,8 +42,8 @@ function trace(value) {
 
 function loadApp() {
     if (navigator.onLine) {
-        // var url = "http://rla.io/screen";
-        var url = "http://192.168.1.56:3000/screen";
+        var url = "http://rla.io/screen";
+        // var url = "http://192.168.1.56:3000/screen";
         var target = "_self";
         var options = "location=no,hidden=yes,zoom=no,hardwareback=no,toolbar=no,allowInlineMediaPlayback=yes";
         var ref = cordova.InAppBrowser.open(url, target, options);
